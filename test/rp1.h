c2056e14cdd646b0f9b789c510374b91838b1cb6
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-10-16 21:12:57 +0100

    Change example test to work with only header file

--- test/rp1.h
+++ test/rp1.h
@@ -1,10 +1,16 @@
 #ifndef RPTEST_RP1_H_
 #define RPTEST_RP1_H_
 
-int add(int a, int b);
+int add(int a, int b) {
+  return a + b;
+}
 
-int substract(int a, int b);
+int substract(int a, int b) {
+  return a - b;
+}
 
-int multiply(int a, int b);
+int multiply(int a, int b) {
+  return a * b;
+}
 
 #endif
