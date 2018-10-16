3c714f97836943dce5d854d1933242cac8bcbcc6
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-10-16 20:17:40 +0100

    make minimal own gtests

---
+++ test/rp1.cpp
@@ -0,0 +1,13 @@
+#include "rp1.h"
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int substract(int a, int b) {
+  return a - b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
---
+++ test/rp1.h
@@ -0,0 +1,10 @@
+#ifndef RPTEST_RP1_H_
+#define RPTEST_RP1_H_
+
+int add(int a, int b);
+
+int substract(int a, int b);
+
+int multiply(int a, int b);
+
+#endif
