84a88ba11a71c62821fbf192fe5add22df494d3a
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-10-19 13:03:24 +0100

    Add instructions to README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Testing new RP - tree-based ANN algorithms
+# Tests for MRPT
+
+This repo contains simple tests for [MRPT algorithm](https://github.com/teemupitkanen/mrpt) to aid the development. Basically, we test that when the random seed given for the function which grows the trees is fixed, the query results are always the same. So if anything related to the random vectors of trees is changed, the tests break with a high probability.
+
+To start testing, first clone [googletest](https://github.com/google/googletest.git) project:
+```
+git clone https://github.com/google/googletest.git
+```
+Then edit the directory containing MRPT code , your preferred C++ compiler and a googletest directory (if you cloned it into the root of this directory, then the default directory works) into `make/Makefile`, for example:
+```
+MRPT_DIR = ../../mrpt
+CXX = g++-8
+GTEST_DIR = ../googletest/googletest
+```
+
+Then make and run the tests:
+```
+cd make
+make
+./test_rp1
+```
--- test/test_rp1.cpp
+++ test/test_rp1.cpp
@@ -1,8 +1,3 @@
-// Step 1. Include necessary header files such that the stuff your
-// test logic needs is declared.
-//
-// Don't forget gtest.h, which declares the testing framework.
-
 #include <random>
 
 #include "rp1.h"
@@ -10,50 +5,8 @@
 #include "Mrpt.h"
 #include "Eigen/Dense"
 
-// Step 2. Use the TEST macro to define your tests.
-//
-// TEST has two parameters: the test case name and the test name.
-// After using the macro, you should define your test logic between a
-// pair of braces.  You can use a bunch of macros to indicate the
-// success or failure of a test.  EXPECT_TRUE and EXPECT_EQ are
-// examples of such macros.  For a complete list, see gtest.h.
-//
-// <TechnicalDetails>
-//
-// In Google Test, tests are grouped into test cases.  This is how we
-// keep test code organized.  You should put logically related tests
-// into the same test case.
-//
-// The test case name and the test name should both be valid C++
-// identifiers.  And you should not use underscore (_) in the names.
-//
-// Google Test guarantees that each test you define is run exactly
-// once, but it makes no guarantee on the order the tests are
-// executed.  Therefore, you should write your tests in such a way
-// that their results don't depend on their order.
-//
-// </TechnicalDetails>
-
 
 namespace {
-
-TEST(TrivialTest, Addition) {
-  EXPECT_EQ(add(1,1), 1+1);
-  EXPECT_TRUE(add(1,1) == 1+1);
-  EXPECT_GT(add(1,1), 1);
-  EXPECT_LT(add(1,1), 3);
-}
-
-TEST(TrivialTest, Substraction) {
-  EXPECT_EQ(substract(1,1), 1-1);
-  EXPECT_TRUE(substract(1,1) == 1-1);
-}
-
-TEST(AnotherTrivialTest, Multiplication) {
-  EXPECT_EQ(multiply(2,2), 2*2);
-  EXPECT_EQ(multiply(0,2), 0);
-}
-
 
 class QueryTest : public testing::Test {
   protected:
@@ -106,7 +59,7 @@ class QueryTest : public testing::Test {
 
 // Test that the nearest neighbors returned by the index
 // are same as before when a seed for rng is fixed
-TEST_F(QueryTest, DenseTrees) {
+TEST_F(QueryTest, Parameters) {
   int n_trees = 10, depth = 6, votes = 1, k = 5;
   float density = 1;
 
@@ -217,15 +170,3 @@ TEST_F(QueryTest, ExactKnn) {
 
 
 }
-
-// Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
