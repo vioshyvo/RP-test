10612c1405eba77a0f316e6c4d98c82af70286b7
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-12-03 08:48:08 +0000

    Add slurm wrapper for mnist

---
+++ timing/slurm-wrappers/mnist-mrpt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=mnist-mrpt
+#SBATCH -o ../script-output/mnist-mrpt.txt
+#SBATCH -c 1
+#SBATCH -p test
+#SBATCH -t 00:30:00
+#SBATCH --mem=2G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
+
+if[ $# -ne 1 ]; then
+  echo "Usage ./mnist-mrpt.sh <post-fix>"
+  exit
+fi
+
+cd ..
+cd ann-benchmarks
+srun ./comparison.sh mnist "$1"
