9b35867cc95f0a8e0fdf3192d3d841f34b424d3b
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-12-08 10:14:08 +0000

    Predict distribution of votes of 1-nn

--- timing/tools/read_votes.R
+++ timing/tools/read_votes.R
@@ -8,23 +8,28 @@ read_votes <- function(lines, n_test = 100) {
   k <- as.integer(strsplit(lines[1], " ")[[1]][1])
   split_lines <- strsplit(lines, " ")
 
-  votes <- list()
+  votes <- cs_sizes <- list()
   line <- lines[1]
   c <- 1
   par <- matrix(as.integer(strsplit(line, ' ')[[1]]), nrow = 1)
   while(!is.na(line)) {
     c <- c + 1
     votes_tmp <- matrix(integer(n_test * k), nrow = n_test, ncol = k)
+    cs_sizes_tmp <- integer(n_test)
     for(i in 1:n_test) {
-      votes_tmp[i, ] <- as.integer(split_lines[[c]])
+      split <- split_lines[[c]]
+      votes_tmp[i, ] <- as.integer(split[-length(split)])
+      cs_sizes_tmp[i] <- as.integer(split[length(split)])
       c <- c + 1
     }
-    votes[[length(votes) + 1]] <- votes_tmp
+    crnt_idx <- length(votes) + 1
+    votes[[crnt_idx]] <- votes_tmp
+    cs_sizes[[crnt_idx]] <- cs_sizes_tmp
     line <- lines[c]
     if(!is.na(line)) par <- rbind(par, as.integer(strsplit(line, ' ')[[1]]))
   }
   parameters <- as.data.frame(par)
   names(parameters) <- c('k', 'n_trees', 'depth', 'v')
   
-  list(parameters = parameters, votes = votes) 
+  list(parameters = parameters, votes = votes, cs_sizes = cs_sizes) 
 }
--- timing/votes.R
+++ timing/votes.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("tools/read_votes.R")
 
@@ -8,16 +9,33 @@ votes_list <- read_votes(read_votes_file(paste(dir, 'votes_100', sep = '/')), n_
 top_votes <- read_votes(read_votes_file(paste(dir, 'top_votes_100', sep = '/')), n_test)$votes
 votes <- votes_list$votes
 par <- votes_list$parameters
+cs_sizes <- votes_list$cs_sizes
 
-i <- 10
+i <- 8
 v <- votes[[i]]
 t <- top_votes[[i]]
+s <- cs_sizes[[i]]
 par[i, ]
 summary(v[, 1])
+summary(t[, 1])
 # v[ ,1]
 
 vmean <- colMeans(v)
 barplot(vmean, col = 'skyblue', main = 'mean votes for 100-nn')
 
-v[1:30, 1:10]
-t[1:30, 1:10]
+cbind(v[1:20, 1:10], s[1:20])
+cbind(t[1:20, 1:10], s[1:20])
+
+######################################################
+# regression model for vote count of 1-nn
+
+target_recall <- .9
+df <- data.frame(x = t[ ,1], y = v[ ,1])
+m <- glm(y ~ x, family="poisson", data = df)
+
+
+pred <- predict.glm(m, type = 'response')
+df$q <- qpois(1 - target_recall, pred)
+df$s <- df$y >= df$q
+
+mean(df$s)
